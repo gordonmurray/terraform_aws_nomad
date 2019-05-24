e19087c587c6d6a931c0d8ce3a9e7dc8500a04d1
Gordon Murray <gordon@gordonmurray.com>
2019-05-24 22:03:13 +0100

    added a specific nomad security group

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,14 +6,15 @@ data "aws_ami" "ubuntu" {
     values = ["nomad*"]
   }
 
-  owners = ["016230046494"]
+  owners = ["${var.owner_id}"]
 
 }
 
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "m5.large"
-  key_name      = "gordonmurray"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "m5.large"
+  key_name               = "gordonmurray"
+  vpc_security_group_ids = ["${aws_security_group.nomad_sg.id}"]
 
   tags = {
     Name = "Nomad"
